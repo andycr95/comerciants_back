86e92becf12c0173f8b90eb1c515aebfa1aedf76
andycr95 <andycr95@icloud.com>
2025-01-17 20:28:24 -0500

    Initial commit

---
+++ src/main/java/com/andycaicedo/comerciants/ComerciantsApplication.java
@@ -0,0 +1,13 @@
+package com.andycaicedo.comerciants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComerciantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComerciantsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andycaicedo/comerciants/ComerciantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andycaicedo.comerciants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComerciantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
